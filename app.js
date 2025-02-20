f09c2390aab4dfe74d2a6bd1499dd93ef483bab5
Areej-Yaffar <areejaini@gmail.com>
2025-02-20 13:24:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/data', (req, res) => {
     const sampleData = {
-        message: "Hola, este es un ejemplo de datos desde el servidor."
+        message: "Holi, este es un ejemplo de datos desde el servidor."
     };
     res.json(sampleData);
+});
+
+app.post('/auth', (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'usuario' && password === 'contraseña') {
+        res.send('Login exitoso');
+    } else {
+        res.send('Usuario o contraseña incorrectos');
+    }
 });
 
 app.listen(PORT, () => {
